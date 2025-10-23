15547034dad2254d27be2d7684db0fb511e3b2bc
Damar2121 <damar.vivoy51@gmail.com>
2025-10-23 15:48:13 +0700

    warna textnya kuning

--- app/src/main/java/com/example/praktikum3/UI_110.kt
+++ app/src/main/java/com/example/praktikum3/UI_110.kt
@@ -74,6 +74,7 @@ fun ActivitasPertama(modifier: Modifier){
                     Text(
                         stringResource(R.string.alamat),
                         fontSize = 20.sp,
+                        color = Color.Yellow,
                     )
 
 
