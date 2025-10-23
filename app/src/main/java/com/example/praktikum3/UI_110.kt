f2d77a86e1fe987034e05b93803abfddb8bb365b
Damar2121 <damar.vivoy51@gmail.com>
2025-10-23 15:53:35 +0700

    membuat text dengan memanggil copy

--- app/src/main/java/com/example/praktikum3/UI_110.kt
+++ app/src/main/java/com/example/praktikum3/UI_110.kt
@@ -84,6 +84,10 @@ fun ActivitasPertama(modifier: Modifier){
         Box(
             modifier = Modifier
                 .fillMaxSize()
-        )
+        ){
+            Text(
+                stringResource(R.string.Copy)
+            )
+        }
     }
 }
